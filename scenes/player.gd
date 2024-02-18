c941c1d313b512fa589a4b9b18a80e7037e516fc
Matthew <monkeytonk115@gmail.com>
2024-02-18 11:50:47 +0000

    update player respawning

--- scenes/player.gd
+++ scenes/player.gd
@@ -9,7 +9,6 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var equipped_weapon
 
 var loadout = ["res://scenes/weapons/paintgun.tscn"]
-var _respawn_point : Vector3
 
 func _enter_tree():
 	# Can only change multiplayer authority from inside _enter_tree()
@@ -46,11 +45,7 @@ func _unhandled_input(event):
 
 func _physics_process(delta):
 	if not is_multiplayer_authority(): return
-	
-	if _respawn_point != Vector3.INF:
-		global_transform.origin = _respawn_point
-		_respawn_point = Vector3.INF
-	
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -71,7 +66,7 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-	
+
 	if equipped_weapon:
 		if equipped_weapon.automatic:
 			if Input.is_action_pressed("primary_fire"):
@@ -91,3 +86,8 @@ func equip(wep : String):
 	equipped_weapon = load(wep).instantiate()
 	$humanoid/hand_right.add_child(equipped_weapon)
 	equipped_weapon.transform = equipped_weapon.find_child("attachment_0").transform.inverse()
+
+
+@rpc("any_peer", "call_local")
+func respawn(respawn_point):
+	global_transform.origin = respawn_point
