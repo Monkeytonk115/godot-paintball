9497515358237ecfefe56fda30dec9a341e6fa12
Matthew <monkeytonk115@gmail.com>
2024-03-06 19:29:02 +0000

    update flag capture zone logic

--- scenes/flag.gd
+++ scenes/flag.gd
@@ -17,7 +17,9 @@ func _ready():
 
 func _physics_process(delta):
 	if self.holder:
-		self.global_transform = self.holder.get_node("flagCarryPoint").global_transform
+		var carryPoint = self.holder.get_node_or_null("flagCarryPoint")
+		if carryPoint:
+			self.global_transform = carryPoint.global_transform
 
 
 func set_team(team):
@@ -27,10 +29,10 @@ func set_team(team):
 	match team:
 		Team.GREEN:
 			new_m.albedo_texture = load("res://addons/kenney_prototype_textures/green/texture_08.png")
-			$Area3D.collision_mask = 4 + 256
+			$Area3D.collision_mask = 4
 		Team.PURPLE:
 			new_m.albedo_texture = load("res://addons/kenney_prototype_textures/purple/texture_08.png")
-			$Area3D.collision_mask = 2 + 128
+			$Area3D.collision_mask = 2
 	$flag.set_surface_override_material(0, new_m)
 
 
@@ -45,21 +47,43 @@ func _on_area_3d_body_entered(body):
 		self.holder = body
 		self.holder.player_hit.connect(drop_flag)
 		$Timer.stop()
+		
+		# scan for capture zones
+		match self._team:
+			Team.GREEN:
+				$Area3D.collision_mask = 128 # 128 is greenFlag Capture Layer (the zone in purple spawn)
+			Team.PURPLE:
+				$Area3D.collision_mask = 256 # 265 is purpleFlag Capture Layer (the zone in green spawn)
 
 
 func drop_flag(_a, _b):
 	self.holder = null
 	dropped.emit(self._team)
 	$Timer.start(30)
+	# scan for players
+	match self._team:
+		Team.GREEN:
+			$Area3D.collision_mask = 4 # 4 is purplePlayers
+		Team.PURPLE:
+			$Area3D.collision_mask = 2 # 2 is greenPlayers
 
 
+# Respawn the flag when it times out
 func _on_timer_timeout():
 	self.global_transform = self._spawn
 	returned.emit(self._team)
+	# scan for players
+	match self._team:
+		Team.GREEN:
+			$Area3D.collision_mask = 4 # 4 is purplePlayers
+		Team.PURPLE:
+			$Area3D.collision_mask = 2 # 2 is greenPlayers
 
 
 func _on_area_3d_area_entered(area):
 	print(area)
+	# stop scanning for collisions
+	$Area3D.collision_mask = 0
 	captured.emit(self._team)
 	self.global_transform.origin = Vector3(0, -10, 0)
 	$Timer.start(8)
