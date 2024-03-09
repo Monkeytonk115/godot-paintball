43af5352dcfdac51c3051c883657c2fd2db90ec8
Matthew <monkeytonk115@gmail.com>
2024-03-09 14:24:30 +0000

    update capture point logic

--- scenes/capture_point.gd
+++ scenes/capture_point.gd
@@ -7,20 +7,32 @@ signal captured(team)
 
 var UncapturedMaterial : StandardMaterial3D
 
+# Players on the capture point
 var pointPlayers = {}
-var pointPoints = 0
 var greenCount = 0
 var purpleCount = 0
 
+# Capture progress variables
+var greenCapture = 0
+var purpleCapture = 0
+
+var _team = Team.SPECTATOR
+
 func _ready():
-	DebugOverlay.add_property(self, "pointPoints", "")
 	DebugOverlay.add_property(self, "greenCount", "")
 	DebugOverlay.add_property(self, "purpleCount", "")
+	
+	DebugOverlay.add_property(self, "greenCapture", "")
+	DebugOverlay.add_property(self, "purpleCapture", "")
+	
+
+	_team = Team.SPECTATOR
+
 	var base = StandardMaterial3D.new()
 	base.albedo_texture = preload("res://addons/kenney_prototype_textures/light/texture_07.png")
 	base.uv1_triplanar = true
 	$capture_point/Circle.set_surface_override_material(0, base)
-	
+
 	UncapturedMaterial = StandardMaterial3D.new()
 	UncapturedMaterial.albedo_texture = preload("res://addons/kenney_prototype_textures/dark/texture_07.png")
 	UncapturedMaterial.uv1_triplanar = true
@@ -61,6 +73,9 @@ func _update_playercount():
 
 
 func team_captured(team):
+	self._team = team
+	self.greenCapture = 0
+	self.purpleCapture = 0
 	match team:
 		Team.GREEN:
 			$capture_point/Circle.set_surface_override_material(1, GreenMaterial)
@@ -74,24 +89,43 @@ func team_captured(team):
 func _process(delta):
 	match [greenCount, purpleCount]:
 		[0, 0]:
-			#print("nobody on point")
-			pass
+			# If nobody is standing on the point, then capture progress decreases to 0 for both
+			# They decrease by 0.5 per second
+			greenCapture  = max(0, greenCapture  - (0.5 * delta))
+			purpleCapture = max(0, purpleCapture - (0.5 * delta))
 		[var g, 0]:
-			#print(g, " green players")
-			pointPoints = min(pointPoints + (_player_to_speed(g) * delta), 20)
+			# Only green players on the point decreases purple's capture progress
+			# We add 1 to the player count to give uncapturing progress a little more speed
+			purpleCapture = max(0, purpleCapture - (_player_to_speed(g + 1) * delta))
+			# If purple's capture progress is 0
+			# and Green team does NOT own the capture point
+			# then we also increase green's capture progress
+			if (purpleCapture < 0.01) and (self._team != Team.GREEN):
+				greenCapture = min(20, greenCapture + (_player_to_speed(g) * delta))
+				if (greenCapture > 19.99):
+					team_captured(Team.GREEN)
 		[0, var p]:
-			#print(p, " purple players")
-			pointPoints = max(pointPoints - (_player_to_speed(p) * delta), -20)
+			# Only purple players on the point decreases green's capture progress
+			# We add 1 to the player count to give uncapturing progress a little more speed
+			greenCapture = max(0, greenCapture - (_player_to_speed(p + 1) * delta))
+			# If green's capture progress is 0
+			# and Purple team does NOT own the capture point
+			# then we also increase purple's capture progress
+			if (greenCapture < 0.01) and (self._team != Team.PURPLE):
+				purpleCapture = min(20, purpleCapture + (_player_to_speed(p) * delta))
+				if (purpleCapture > 19.99):
+					team_captured(Team.PURPLE)
 		[var g, var p]:
-			#print(g, " green players and ", p, " purple players")
+			# If both teams are standing then capture progress does not change
 			pass
-	#print(pointPoints)
 
 
 func _exit_tree():
-	DebugOverlay.remove_property(self, "pointPoints")
 	DebugOverlay.remove_property(self, "greenCount")
 	DebugOverlay.remove_property(self, "purpleCount")
+	
+	DebugOverlay.remove_property(self, "greenCapture")
+	DebugOverlay.remove_property(self, "purpleCapture")
 
 
 func _player_to_speed(player_count : int):
