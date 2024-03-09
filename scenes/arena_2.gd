71afbde6dfd0a02b5e5d97a495ec0833d33904d4
Matthew <monkeytonk115@gmail.com>
2024-03-09 14:25:37 +0000

    update signals and fix spelling in arena

--- scenes/arena_2.gd
+++ scenes/arena_2.gd
@@ -9,14 +9,10 @@ func game_start():
 	ticketsPurple = 100
 
 
-func _on_caputure_point_entered(node):
-	pass # Replace with function body.
-
-
-func _on_caputure_point_exited(node):
-	pass # Replace with function body.
-
-
 # Timer runs every 1 second
 func _on_timer_timeout():
+	pass # Replace with function body.
+
+
+func _on_caputure_point_captured(team):
 	pass # Replace with function body.
